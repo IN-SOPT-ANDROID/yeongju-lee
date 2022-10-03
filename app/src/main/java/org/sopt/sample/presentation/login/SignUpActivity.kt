527ba05574d741f495bda578ddb348e00d0ea587
이빵주 <yungju9982@naver.com>
2022-10-04 00:38:22 +0900

    #1 SignUpActivity 생성

---
+++ app/src/main/java/org/sopt/sample/presentation/login/SignUpActivity.kt
@@ -0,0 +1,46 @@
+package org.sopt.sample.presentation.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.sopt.sample.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initSignUpBtnOnClickListener()
+    }
+
+    private fun initSignUpBtnOnClickListener() {
+        binding.btnSignUp.setOnClickListener {
+            if (isCheckId() && isCheckPwd() && isCheckMbti()) {
+                putUserInfo()
+                finish()
+            } else Snackbar.make(binding.root, "입력한 정보를 확인해주세요.", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun putUserInfo() {
+        val intent = Intent(this, SignInActivity::class.java)
+        intent.putExtra("userId", binding.etSignUpId.text.toString())
+        intent.putExtra("userPwd", binding.etSignUpPwd.text.toString())
+        intent.putExtra("userMbti", binding.etSignUpMbti.text.toString())
+        setResult(RESULT_OK, intent)
+    }
+
+    private fun isCheckId(): Boolean {
+        return binding.etSignUpId.text.length in 6..10
+    }
+
+    private fun isCheckPwd(): Boolean {
+        return binding.etSignUpPwd.text.length in 8..12
+    }
+
+    private fun isCheckMbti(): Boolean {
+        return binding.etSignUpMbti.text.length == 4
+    }
+}
