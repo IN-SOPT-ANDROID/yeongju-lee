7fe780d785ac782f58d7c09a756a33f2b6c2e4d0
이빵주 <yungju9982@naver.com>
2022-12-01 02:40:50 +0900

    에러뷰 액티비티 생성

---
+++ app/src/main/java/org/sopt/sample/presentation/error/ErrorActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.sample.presentation.error
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.sample.R
+
+class ErrorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
